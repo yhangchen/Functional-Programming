be452c693f670e54c8ac107148d7a2ff5505d97f
Yihang Chen <yihang.chen@epfl.ch>
2021-11-13 22:34:26 +0100

    streams

--- src/main/scala/streams/Solver.scala
+++ src/main/scala/streams/Solver.scala
@@ -8,7 +8,7 @@ trait Solver extends GameDef:
   /**
    * Returns `true` if the block `b` is at the final position
    */
-  def done(b: Block): Boolean = ???
+  def done(b: Block): Boolean = ((b.b1 == goal) && (b.b2 == goal))
 
   /**
    * This function takes two arguments: the current block `b` and
@@ -26,7 +26,7 @@ trait Solver extends GameDef:
    * It should only return valid neighbors, i.e. block positions
    * that are inside the terrain.
    */
-  def neighborsWithHistory(b: Block, history: List[Move]): LazyList[(Block, List[Move])] = ???
+  def neighborsWithHistory(b: Block, history: List[Move]): LazyList[(Block, List[Move])] = for ne <- b.legalNeighbors.to(LazyList) yield (ne._1, ne._2 :: history)
 
   /**
    * This function returns the list of neighbors without the block
@@ -34,7 +34,7 @@ trait Solver extends GameDef:
    * make sure that we don't explore circular paths.
    */
   def newNeighborsOnly(neighbors: LazyList[(Block, List[Move])],
-                       explored: Set[Block]): LazyList[(Block, List[Move])] = ???
+                       explored: Set[Block]): LazyList[(Block, List[Move])] = for ne <- neighbors if !explored.contains(ne._1) yield ne
 
   /**
    * The function `from` returns the lazy list of all possible paths
@@ -60,18 +60,21 @@ trait Solver extends GameDef:
    * construct the correctly sorted lazy list.
    */
   def from(initial: LazyList[(Block, List[Move])],
-           explored: Set[Block]): LazyList[(Block, List[Move])] = ???
+           explored: Set[Block]): LazyList[(Block, List[Move])] = if initial.isEmpty then LazyList() else {
+            val new_neighbor =  newNeighborsOnly(neighborsWithHistory(initial.head._1, initial.head._2), explored)
+            initial.head #:: from(initial.tail #::: new_neighbor, explored ++ new_neighbor.map(i => i._1))
+           }
 
   /**
    * The lazy list of all paths that begin at the starting block.
    */
-  lazy val pathsFromStart: LazyList[(Block, List[Move])] = ???
+  lazy val pathsFromStart: LazyList[(Block, List[Move])] = from(LazyList((startBlock, List[Move]())), Set(startBlock))
 
   /**
    * Returns a lazy list of all possible pairs of the goal block along
    * with the history how it was reached.
    */
-  lazy val pathsToGoal: LazyList[(Block, List[Move])] = ???
+  lazy val pathsToGoal: LazyList[(Block, List[Move])] = pathsFromStart.filter(i => done(i._1))
 
   /**
    * The (or one of the) shortest sequence(s) of moves to reach the
@@ -81,4 +84,4 @@ trait Solver extends GameDef:
    * the first move that the player should perform from the starting
    * position.
    */
-  lazy val solution: List[Move] = ???
+  lazy val solution: List[Move] = if pathsToGoal.isEmpty then Nil else pathsToGoal.map(i => i._2).head.reverse // remember to reverse
--- src/main/scala/streams/StringParserTerrain.scala
+++ src/main/scala/streams/StringParserTerrain.scala
@@ -50,7 +50,12 @@ trait StringParserTerrain extends GameDef:
    * a valid position (not a '-' character) inside the terrain described
    * by `levelVector`.
    */
-  def terrainFunction(levelVector: Vector[Vector[Char]]): Pos => Boolean = ???
+  def terrainFunction(levelVector: Vector[Vector[Char]]): Pos => Boolean = (pos : Pos) => {
+    if pos.col < 0 || pos.row < 0 then false
+    else if pos.row >= levelVector.length then false
+    else if pos.col >= levelVector(pos.row).length then false
+    else List('S', 'T', 'o').contains(levelVector(pos.row)(pos.col))
+  }
 
   /**
    * This function should return the position of character `c` in the
@@ -60,7 +65,11 @@ trait StringParserTerrain extends GameDef:
    * Hint: you can use the functions `indexWhere` and / or `indexOf` of the
    * `Vector` class
    */
-  def findChar(c: Char, levelVector: Vector[Vector[Char]]): Pos = ???
+  def findChar(c: Char, levelVector: Vector[Vector[Char]]): Pos = {
+    val x = levelVector.indexWhere(_.contains(c))
+    val y = levelVector(x).indexOf(c)
+    Pos(x, y)
+  }
 
   private lazy val vector: Vector[Vector[Char]] =
     Vector(level.split("\r?\n").map(str => Vector(str*)).toIndexedSeq*)
